51cc6a6be15acfd5bf0e39dd5a3807fcf87b6624
杨佳晟 <494268485@qq.com>
2023-02-23 10:40:48 +0800

    test

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 include './vendor/autoload.php';
-echo 11;
+echo 112;
 //$a = \Timerange\RangeDate::set(2020,true)->getMonth();
 $a = \Timerange\RangeTime::set(['2020-04-26','2020-04-27'],true)->getHours('8:30',['08:30','17:00'],['12:30','13:30']);
 echo '<pre>';
